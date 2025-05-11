8f13e987b9b7f002955ab7faf24350203e50391b
pratik0030 <136796023+pratik0030@users.noreply.github.com>
2025-05-11 18:42:47 +0530

    pratik.py

--- pratik.py
+++ pratik.py
@@ -1,7 +1,4 @@
 import streamlit as st
 
 st.title("🚨 Crime Rate Dashboard")
-st.write("Hello! This is a basic Streamlit app.")
-git add app.py
-git commit -m "Add app.py"
-git push
+st.write("This is a simple Streamlit app.")
